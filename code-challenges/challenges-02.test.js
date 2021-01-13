64b29bded7c0876053efc38629e40a0787a35aa7
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-01-12 22:02:43 -0800

    challenges 5 and 6 complete

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -85,6 +85,9 @@ console.log(people[1].isAuthor) prints true
 
 const setStatusAsAuthor = (people) => {
   // Solution code here...
+  people.forEach((person) => {
+    person.isAuthor = true;
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -104,7 +107,11 @@ console.log(a) prints [1, 2, 3, 4]
 
 const append = (arr1, arr2) => {
   // Solution code here...
-
+  const arrAppend = arr1;
+  arr2.forEach((arrayValue) => {
+    const addValue = arrayValue;
+    arrAppend.push(addValue);
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -152,7 +159,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add a property to every object in an array', () => {
     const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
     setStatusAsAuthor(a);
@@ -163,7 +170,7 @@ xdescribe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
