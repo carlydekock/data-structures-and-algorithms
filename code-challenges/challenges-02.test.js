1f9a034d27d3e6e743b007a9f3e8d63aafcfdba4
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-01-12 21:33:25 -0800

    challenge 3 complete

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -46,6 +46,8 @@ console.log(a) prints [1, 2, 3, 1]
 
 const appendFirstToLast = (arr) => {
   // Solution code here...
+  let first = arr[0];
+  arr.push(first);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -131,7 +133,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should append by modifying the oiginal', () => {
     const a = ['Yes', 'it', 'is'];
     appendFirstToLast(a);
